fb7d0c3d941c057aeeaa9a3ce35896caaf1b3aa1
spyspy <spyhopping101@gmail.com>
2018-11-07 05:44:59 +0800

    happy

--- src/main/java/com.test.jdk8/jdk8test003.java
+++ src/main/java/com.test.jdk8/jdk8test003.java
@@ -1,5 +1,8 @@
 package com.test.jdk8;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class jdk8test003 {
 
     public static void main(String[] args) {
@@ -9,6 +12,18 @@ public class jdk8test003 {
         System.out.println(ti1.getClass().getSuperclass());
         System.out.println(ti1.getClass().getInterfaces()[0]);
 
+        new Thread(()-> System.out.println("hello world")).start();
+
+        List<String> list =  Arrays.asList("Test1","Test2","Test3");
+
+        //list.forEach(it -> System.out.println(it.toUpperCase()));
+
+        list.stream().map(it -> it.toUpperCase()).forEach(it-> System.out.println(it));
+        list.stream().map(String::toUpperCase).forEach(it-> System.out.println(it));
+
+        for(String str : list){
+            System.out.println(str);
+        }
 
     }
 }
---
+++ src/main/java/com.test.jdk8/jdk8test006.java
@@ -0,0 +1,7 @@
+package com.test.jdk8;
+
+public class jdk8test006 {
+    public static void main(String[] args) {
+
+    }
+}
